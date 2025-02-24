1ee8d54711625fa16cdfdd03f38df09012c44128
applecider <arizah.applecider+github@gmail.com>
2025-02-24 14:49:37 -0600

    First Commit! :)

---
+++ FRONTEND/src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/App.css'
+import NavBar from './components/NavBar'
+function App() {
+
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ FRONTEND/src/components/NavBar.tsx
@@ -0,0 +1,43 @@
+// Svgs
+import whatsappSvg from "../assets/img/index/whatsapp.svg";
+import facebookSvg from "../assets/img/index/facebook.svg";
+import instagramSvg from "../assets/img/index/instagram.svg";
+import glassStoreLogo from "../assets/img/index/GLASS.png";
+import searchSvg from "../assets/img/index/search.svg";
+import favoritesSvg from "../assets/img/index/favorites.svg";
+import cotizacionesSvg from "../assets/img/index/clip.svg";
+import userSvg from "../assets/img/index/user.svg";
+// Config
+import { SocialMedia } from "../assets/config/config.json";
+// Styles
+import "../styles/navBar.css";
+function NavBar() {
+  return (
+    <section id="navBar">
+      <article className="navIcons">
+        <a href={SocialMedia.whatsapp} target="_blank">
+          <img src={whatsappSvg} alt="WhatsApp" className="svg" />
+        </a>
+        <a href={SocialMedia.facebook} target="_blank">
+          <img src={facebookSvg} alt="Facebook" className="svg" />
+        </a>
+        <a href={SocialMedia.instagram} target="_blank">
+          <img src={instagramSvg} alt="Instagram" className="svg" />
+        </a>
+      </article>
+      <figure className="logo">
+        <img src={glassStoreLogo} alt="GlassStoreLogo" />
+      </figure>
+      <article className="navIcons">
+        <a id="searchAnchor" href="#">
+          <img src={searchSvg} alt="Buscar Productos" className="svgOptions" />
+        </a>
+        <img src={favoritesSvg} alt="Favoritos" className="svgOptions" />
+        <img src={cotizacionesSvg} alt="Cotizaciones" className="svgOptions" />
+        <img src={userSvg} alt="Usuarios" className="svgOptions" />
+      </article>
+    </section>
+  );
+}
+
+export default NavBar;
